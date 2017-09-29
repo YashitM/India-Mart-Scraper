dbb8a7d4d4f224aaa5220c49be4290aac7319938
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2017-09-30 03:17:05 +0530

    Item Scraping complete, need to export in a dictionary

--- scrape.py
+++ scrape.py
@@ -2,88 +2,67 @@ import requests
 from bs4 import BeautifulSoup
 
 def getCategories():
-	r = requests.get("https://trade.indiamart.com")
-	html = r.content
-
-	soup = BeautifulSoup(html, "html.parser")
-
-	categories = soup.find_all("li", {"class": "dc-mega-li"})
-
+	soup = BeautifulSoup(requests.get("https://trade.indiamart.com").content, "html.parser")
 	categories_dictionary = dict()
 
-	for item in categories:
+	for item in soup.find_all("li", {"class": "dc-mega-li"}):
 		if item.find("a").get("href") != "#":
 			categories_dictionary[item.text.lstrip().rstrip("\n")] = item.find("a").get("href")
-
-	print("[*] categories done")
-
-	other_categories = soup.find_all("li", {"class": "menu-item-131"})
-
-	for item in other_categories:
+	# do the same for 132 and 133
+	for item in soup.find_all("li", {"class": "menu-item-131"}):
 		categories_dictionary[item.text.lstrip().rstrip("\n")] = item.find("a").get("href")
 
+	print("[*] Categories found!")
 	return categories_dictionary
-	# for i in categories_dictionary:
-	# 	print(i + ": " + categories_dictionary[i])
 
 def getSubcategories(categoryName, categoryURL):
-	r = requests.get(categoryURL)
-	html = r.content
-
-	soup = BeautifulSoup(html, "html.parser")
-
-	print ("[+] Finding subCategories of " + categoryName)
-	ulOfSubCategories = soup.find_all("ul", {"class": "grouplist"})
+	soup = BeautifulSoup(requests.get(categoryURL).content, "html.parser")
+	print ("[+] Finding Sub Categories of " + categoryName)
 	subCategories_dictionary = dict()
 
-	for ul in ulOfSubCategories:
+	for ul in soup.find_all("ul", {"class": "grouplist"}):
 		for li in ul.find_all("li"):
 			if li.find("a").get("href") != "#":
 				text = li.text.lstrip().rstrip("\n")
-				# print (text[:text.find("(")])
 				subCategories_dictionary[text[:text.find("(")]] = li.find("a").get("href")
 
 	return subCategories_dictionary
-	# for item in subCategories:
-	# 	pass
-
-	for i in subCategories_dictionary:
-		print (i + ": " + subCategories_dictionary[i])
 
 def getItems(categoryName, subCategoryName, subCategoryURL):
-	r = requests.get(subCategoryURL)
-	html = r.content
-
-	soup = BeautifulSoup(html, "html.parser")
-
+	soup = BeautifulSoup(requests.get(subCategoryURL).content, "html.parser")
 	print ("[+] Getting items in " + categoryName + "->" + subCategoryName)
-
-	#get Divs
-	itemDiv = soup.find_all("div", {"class": "blockdiv"})
+	# itemDiv = soup.find_all("div", {"class": "blockdiv"})
+	itemDiv = soup.find_all("div", {"class": "trade-list"})
 	item_dictionary = dict()
-
 	for div in itemDiv:
 		location = "-"
 		capacity = "-"
-		quantity = "-"
 		quantity_unit = "-"
-		need_for_this = "-" #along with usage
+		quantity = "-"
+		need_for_this = "-"
 		usage = "-"
 		frequency = "-"
-
-
-		itemName = div.find("div",{"class": "d_lm"}).find("p",{"class": "d_f1 mb"}).find("a").text
-		location = div.find("span", {"class": "latestBLBg crdLocation"}).text
-		print(itemName)
-		print(location)
+		# itemName = div.find("div",{"class": "d_lm"}).find("p",{"class": "d_f1 mb"}).find("a").text
+		itemName = div.find("div",{"class": "d_lm"}).find("p",{"class": "d_f1"}).find("a").text
+		# location = div.find("span", {"class": "latestBLBg crdLocation"}).text
+		location = div.find("span", {"class": "bl_ccname location"}).text
 		date = div.find("span", {"class": "dtt updatedTime"}).text
-		print(date)
-		other_details = div.find("div", {"class": "c15 pt4 fs pl"})
-		for table_row in div.find_all("tr"):
-			print(table_row.text)
+		print("Name: " + itemName)
+		print("Location: " + location)
+		print("Date: " + date)
+		other_details = dict()
 
-		
+		if div.find("div", {"class": "c15 pt4 fs pl"}) != None:
+			for table_row in div.find("div", {"class": "c15 pt4 fs pl"}).find_all("tr"):
+				data = table_row.text.replace("\n","").split(":")
+				if data[0] == "Quantity":
+					quantity = data[1].split(" ")[0].lstrip()
+					quantity_unit = data[1].split(" ")[1].lstrip()
+				print(data)
 
+			# other_details["lol"] = data
+
+		print(other_details)
 
 if __name__ == '__main__':
 	categories = getCategories()
@@ -95,12 +74,6 @@ if __name__ == '__main__':
 		if j==2:
 			break
 
-	# for i in subCategories:
-	# 	print (i)
-	# 	print (subCategories[i])
-
 	for i in subCategories:
 		for j in subCategories[i]:
-			# print(i + "   i")
-			# print(subCategories[i][j])
 			getItems(i,j,subCategories[i][j])
